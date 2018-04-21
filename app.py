aae0be2d9e8bca9c9fd63aa396a9e0475dfece73
agarwalkirti <akirti302@gmail.com>
2018-04-21 13:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ def webhook():
         res = processPNRStatus(req)
     if req.get("result").get("action") == "stationName":
         res = processStationName(req)
+    if req.get("result").get("action") == "arrival":
+        res = processArrival(req)
     res = json.dumps(res, indent=4)
 
     r = make_response(res)
@@ -347,9 +349,30 @@ def processStationName(req):
             "source": "webhook-dm"
             }
     return reply
-  
+#Train Arrival
+def processArrival(req):
+    if req.get("result").get("action") != "arrival":
+        return {}
+    baseurl = "https://api.railwayapi.com/v2/arrivals/station/GZB/hours/4"
+    remain = "/apikey/"+apikey
+    yql_url = baseurl + remain
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResultArrival(data)
+    return res  
 # ----------------------------------------json data extraction functions---------------------------------------------------
-
+def makeWebhookResultArrival(data):
+    speech = ""
+    for code in data['trains']:
+        speech =  speech +"Train Name: "code['name']  +",  "
+#    speech = speech.rstrip('>')
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "webhook-dm"
+    }
 def makeWebhookResultStatus(data):
     speech = data.get('position')
     if data.get('response_code') == 210:
